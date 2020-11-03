78b6bbcca598492d497a6f26fcb16677194c05c6
tunarider <tunarider@tunaground.net>
2020-11-03 14:06:33 +0900

    output to stderr -> stdout

--- internal/output/output.go
+++ internal/output/output.go
@@ -8,19 +8,23 @@ import (
 )
 
 func Ok(msg string) cli.ExitCoder {
-	return cli.Exit(fmt.Sprintf("OK: %s", msg), nagios.StateToInt(nagios.StateOk))
+	fmt.Printf("OK: %s", msg)
+	return cli.Exit("", nagios.StateToInt(nagios.StateOk))
 }
 
 func Warning(msg string) cli.ExitCoder {
-	return cli.Exit(fmt.Sprintf("Waring: %s", msg), nagios.StateToInt(nagios.StateWarning))
+	fmt.Printf("Warning: %s", msg)
+	return cli.Exit("", nagios.StateToInt(nagios.StateWarning))
 }
 
 func Critical(msg string) cli.ExitCoder {
-	return cli.Exit(fmt.Sprintf("Critical: %s", msg), nagios.StateToInt(nagios.StateCritical))
+	fmt.Printf("Critical: %s", msg)
+	return cli.Exit("", nagios.StateToInt(nagios.StateCritical))
 }
 
 func Unknown(msg string) cli.ExitCoder {
-	return cli.Exit(fmt.Sprintf("Unknown: %s", msg), nagios.StateToInt(nagios.StateUnknown))
+	fmt.Printf("Unknown: %s", msg)
+	return cli.Exit("", nagios.StateToInt(nagios.StateUnknown))
 }
 
 func MakeOutput(msg string, performances []string) string {
