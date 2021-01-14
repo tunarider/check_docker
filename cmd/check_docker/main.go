5fa0a0c175bfc6a8a3b167c76bc923370b1eb3d4
tunarider <tunarider@tunaground.net>
2021-01-14 13:09:40 +0900

    version v0.3.1

--- cmd/check_docker/main.go
+++ cmd/check_docker/main.go
@@ -32,7 +32,7 @@ func main() {
 	app := cli.App{
 		Name:    "check_docker",
 		Usage:   "Check docker status",
-		Version: "v0.3.0",
+		Version: "v0.3.1",
 		Commands: []*cli.Command{
 			{
 				Name:    "node",
